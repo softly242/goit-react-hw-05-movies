3fcf27866b7b76920a4ba413ec4a3d96ff4e18d7
softly242 <113473257+softly242@users.noreply.github.com>
2023-06-23 18:49:57 +0000

    Deploying to gh-pages from @ softly242/goit-react-hw-05-movies@6dee39d2da1b6e67cc1e5b92acb71a4d85a14113 🚀

---
+++ static/js/922.056eab1d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[922],{8922:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(184);function c(){return(0,r.jsxs)("main",{style:{textAlign:"center"},children:[(0,r.jsx)("b",{style:{fontSize:64},children:"404"}),(0,r.jsx)("p",{children:"Sorry, we couldn't find that page :("})]})}}}]);
+//# sourceMappingURL=922.056eab1d.chunk.js.map
